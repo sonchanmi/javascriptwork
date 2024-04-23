86f18aa4eedb484a3c5012702e30255506c39d49
손찬미 <sonchanmi@naver.com>
2024-04-23 15:23:50 +0900

    1.0 init

---
+++ java_spring0418/js/common.js
@@ -0,0 +1,5 @@
+function Print(){
+
+    alert('print');
+
+}
---
+++ java_spring0418/js/lesson1.js
@@ -0,0 +1,14 @@
+function Lesson1(){
+
+
+// 변수:변수는 오른쪽에서 왼쪽으로 값이 이동한다,오른쪽에 문자열이 왼쪽 name변수에 저장된다
+// 상수: 변하지 않는다 
+// jacascript var: 모든타입을 받을수 있다(모든타입의 부모 var)
+// console 어떤얘기에 대한 것을 기록해서 남기겠다 
+
+var name='아무개';
+
+console.log('name');
+
+
+}
